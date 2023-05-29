d540a60a88fa3fe751a1d066ddff269cd5e6741f
root <yash.joon@freecharge.com>
2023-05-29 08:36:31 +0530

    UseState-EventHandling

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
+import React, {useState} from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
     
-    const expenseTitle = props.title;
+    let expenseTitle = props.title;
     const expenseAmount = props.amount;
+    const [title, setTitle] = useState(expenseTitle);
+
+    const clickHandler = () => {
+        setTitle('Updated!!')
+    };
 
     return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{expenseTitle}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${expenseAmount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 }
